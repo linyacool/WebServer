81a5c8c85ffc577cf55333c9f1d5d2f81d9a61f2
linyacool <xxbbb@vip.qq.com>
2018-02-10 15:14:12 +0800

    change dir

--- old_version_0.4/util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdlib>
-
-ssize_t readn(int fd, void *buff, size_t n);
-ssize_t writen(int fd, void *buff, size_t n);
-void handle_for_sigpipe();
-int setSocketNonBlocking(int fd);
