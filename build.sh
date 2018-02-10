8e9382013cedcd688bfad1c1c68de2a80d9ae15a
linyacool <xxbbb@vip.qq.com>
2018-02-10 13:54:27 +0800

    auto build

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_TYPE=${BUILD_TYPE:-release}
+
+mkdir -p $BUILD_DIR/$BUILD_TYPE \
+    && cd $BUILD_DIR/$BUILD_TYPE \
+    && cmake \
+            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+            $SOURCE_DIR \
+    && make $*
